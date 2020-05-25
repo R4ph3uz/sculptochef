f51937061e1a42c0d8f8afb3b1b6a437fc191e73
R4ph3uz <r4ph3uz@gmail.com>
2020-05-25 10:56:03 +0200

    'initialisation'

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Sculptochef - Accueil</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./private/css/style.css">
+</head>
+
+<body>
+
+    <div class="container--accueil">
+        <div class="container--content">
+            <header>
+            <div class="logo"><img src="./images" alt=""></div>
+            </header>
+        </div>
+    </div>
+
+</body>
+
+</html>
